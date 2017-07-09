15e864d810ebc3870cc38b38811fc77d354f718b
paulmorello <paulmorello1@gmail.com>
2017-07-09 11:34:05 -0700

    tracking arrow clicks to about section

--- src/ArrowDown.js
+++ src/ArrowDown.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 
 class ArrowDown extends Component {
 
+  trackClick() {
+    // Tracking clicks on Navbar Links
+    woopra.track('Site View', {
+      url: window.location.pathname,
+      title: document.title,
+      section: 'About'
+    })
+  }
+
   render() {
     return (
-      <div className="header-arrow-container">
+      <div
+      className="header-arrow-container"
+      onClick={ this.trackClick } >
         <a href="#about"><i className="fa fa-angle-double-down fa-4x" aria-hidden="true"></i></a>
       </div>
     )
