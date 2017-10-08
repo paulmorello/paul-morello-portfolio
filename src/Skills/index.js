fe417c3779a6349066ce783e0adb482a5dad31f5
paulmorello <paulmorello1@gmail.com>
2017-10-08 16:21:21 -0700

    code refactoring

--- src/Skills/index.js
+++ src/Skills/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { PieChart } from 'react-chartkick';
 import SkillsChart from './SkillsChart';
 
 class Skills extends Component {
